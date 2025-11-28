0d0735badccfa3a423a6d193404e25e99c23e7d6
livvyc5353 <oliviamclontz@gmail.com>
2025-11-28 17:40:46 -0500

    database scripts

---
+++ src/fitness_app_users_and_workouts/application_base.py
@@ -0,0 +1,20 @@
+"""Implements behavior common to all application classes.."""
+
+from abc import ABC, abstractmethod
+from fitness_app_users_and_workouts.logging import LoggingService
+from fitness_app_users_and_workouts.settings import Settings
+
+class ApplicationBase(ABC):
+    """Implements ApplicationBase class."""
+    
+    def __init__(self, subclass_name:str, logfile_prefix_name:str)->None:
+        """Instantiate instance."""
+        self._settings = Settings().read_settings_file_from_location()
+        self._logger = LoggingService(subclass_name, logfile_prefix_name)
+        
+       
+    
+
+
+
+    
